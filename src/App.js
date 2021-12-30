14488119efa4c66c2e9f4338bece169faf1beef5
Dulanjana Bandara <dulanjanauop@gmail.com>
2021-12-30 09:43:22 +0530

    Punks data were fetched using axios and stored

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import Header from "./components/Header";
 import CollectionCard from "./components/CollectionCard";
-
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]); // punkListData is going to store all the punks details right in there.
+
+  // useEffect() is used for make an API call
+  useEffect(() => {
+    // accessing data
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xf5C5b34ec9A5aCE3eB3F260049e5510f5204c99c&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+
+      // storing data
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
+
   return (
     <div className="app">
       <Header />
