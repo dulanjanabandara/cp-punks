8895c02f309ff264926697e96e68e1346edd4cee
Dulanjana Bandara <dulanjanauop@gmail.com>
2021-12-30 07:52:20 +0530

    One card was designed completely

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <Header></Header>
+      <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu"
+      />
     </div>
   );
 }
